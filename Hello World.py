43f86fbcf3ba100f49a99717434ac50ef50b861d
hhawke08 <hhawketts@gmail.com>
2025-11-24 11:40:50 +0000

    Add files via upload
    Added additional line to verify age and confirm with the user.

--- Hello World.py
+++ Hello World.py
@@ -4,3 +4,10 @@ print (greeting_phrase)
 # Created a user input request for the user to identify themself.
 user_input = input("Please enter your name : ")
 print("Hello " + user_input + "!")
+new_user_input = input("Please enter your age : ")
+#Created a confirmation line referring to the previously established variables.
+print("Your name is " + user_input + " and you are " + new_user_input + " years old. Is this correct?")
+final_user_input = input()
+
+
+    
