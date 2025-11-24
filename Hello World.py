8147bd99e5b075d00893bd8529b17ed1f67c8db2
hhawke08 <hhawketts@gmail.com>
2025-11-24 11:03:04 +0000

    Add files via upload
    Simple "Hello World" and user input test.

---
+++ Hello World.py
@@ -0,0 +1,6 @@
+# Created variable to store the phrase "Hello World".
+greeting_phrase = ("Hello World")
+print (greeting_phrase)
+# Created a user input request for the user to identify themself.
+user_input = input("Please enter your name : ")
+print("Hello " + user_input + "!")
